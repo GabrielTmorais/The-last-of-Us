e8054b51d7f5c95c6b84352a1d2306123b3d5464
Gabriel <g.tonera10@hotmail.com>
2024-09-12 22:39:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+    
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById('resultados-pesquisa')
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // Se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = `<p class="mensagem-erro">Nada foi encontrado. Você precisa digitar o nome de um personagem!</p>`
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = "";
+
+
+    // Itera sobre cada dados da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        // Se titulo ou descrição includes campoPesquisa
+        if (titulo.includes(campoPesquisa)) {
+            // Cria um novo elemento
+            resultados += `<div class="container">
+            <img class="logo" src="${dado.logo}" alt="Logo vagalumes">
+            <img class="personagem" src="${dado.imagem}" alt="Ellie">
+            <h2 class="titulo">
+                <a href=${dado.link} target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="texto">${dado.descricao}</p>
+        </div>`;
+        }
+       
+    }
+    
+    if (!resultados) {
+       resultados = `<p class="mensagem-erro">Nada foi encontrado</p>`
+    }
+
+    section.innerHTML = resultados 
+}
+
+const audio = document.getElementById('Audio');
+audio.style.display = 'none';
---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo : "Ellie Williams",
+        descricao : "Ellie, protagonista da franquia The Last of Us, é uma jovem complexa e resiliente que amadurece em um mundo pós-apocalíptico brutal. Nascida em um mundo devastado por um fungo que transforma humanos em criaturas infectadas, Ellie carrega consigo uma imunidade única que a torna a última esperança para a humanidade.",
+        link : "https://thelastofus.fandom.com/wiki/Ellie_Williams",
+        imagem : src="../img/ellie.jpg",
+        logo : src="../img/vagalumes.png"
+    },
+    {
+        titulo : "Joel Miller",
+        descricao : "Joel Miller, protagonista de The Last of Us, é um personagem complexo e marcado pela perda. Vivendo em um mundo pós-apocalíptico brutal, Joel desenvolveu um exterior duro e cínico como mecanismo de defesa. Sua jornada com Ellie o força a confrontar seus traumas passados e a reavaliar suas prioridades. A perda de sua filha, Sarah, no início da pandemia, deixou uma ferida profunda em seu coração, transformando-o em um homem solitário e desconfiado.",
+        link : "https://thelastofus.fandom.com/wiki/Joel_Miller",
+        imagem : src="../img/joel.jpg",
+        logo : src="../img/vagalumes.png"
+    },
+    {
+        titulo : "Abby Anderson",
+        descricao : "Abby Anderson, protagonista controversa de The Last of Us Parte II, é uma personagem complexa e multifacetada que desafia as expectativas do jogador. Forte, habilidosa e determinada, Abby é motivada por uma busca implacável por vingança após a perda de seu pai, Jerry, nas mãos de Joel. Sua jornada a leva a um confronto brutal com Ellie e a explorar os limites da moralidade em um mundo pós-apocalíptico.",
+        link : "https://thelastofus.fandom.com/wiki/Abby_Anderson",
+        imagem : src="../img/abby.png",
+        logo : src="../img/vagalumes.png"
+    }
+]
